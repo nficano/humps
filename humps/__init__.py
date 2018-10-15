6e0adf9c4cf0325b21f91a846446cc472283e8f6
Nick Ficano <nficano@gmail.com>
2018-10-15 10:39:11 -0400

    improved import interface

--- humps/__init__.py
+++ humps/__init__.py
@@ -9,3 +9,17 @@ __version__ = '0.1.1'
 __author__ = 'Nick Ficano'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2018 Nick Ficano'
+
+from humps.__main__ import camelize
+from humps.__main__ import decamelize
+from humps.__main__ import depascalize
+from humps.__main__ import pascalize
+
+from humps.__main__ import camelize_keys
+from humps.__main__ import decamelize_keys
+from humps.__main__ import depascalize_keys
+from humps.__main__ import pascalize_keys
+
+from humps.__main__ import is_camelcase
+from humps.__main__ import is_pascalcase
+from humps.__main__ import is_snakecase
