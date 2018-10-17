fc8d362cb2bf56e1ef1865857826ae5f6267aa7b
Nick Ficano <nficano@gmail.com>
2018-10-16 23:38:13 -0400

    Bump version: 0.5.0 → 0.5.1

--- humps/__init__.py
+++ humps/__init__.py
@@ -5,7 +5,7 @@
 Underscore-to-camelCase converter (and vice versa) for strings and dict keys in Python.
 """
 __title__ = 'pyhumps'
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 __author__ = 'Nick Ficano'
 __license__ = 'MIT License'
 __copyright__ = 'Copyright 2018 Nick Ficano'
