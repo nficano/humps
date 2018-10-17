d10123595ccb292b7ef33e7399e9538a99a44de0
Nick Ficano <nficano@gmail.com>
2018-10-17 18:40:53 -0400

    Bump version: 1.0.2 → 1.0.3

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('LICENSE') as readme_file:
     license = readme_file.read()
 
 setup(
-    name='pyhumps', version='1.0.2', author='Nick Ficano',
+    name='pyhumps', version='1.0.3', author='Nick Ficano',
     author_email='nficano@gmail.com', packages=['humps'],
     url='https://github.com/nficano/humps', license=license,
     classifiers=[
