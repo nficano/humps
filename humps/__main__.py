42db3d8a639d5a9d41d07710fbd22a5dfe4a40b6
Nick Ficano <nficano@gmail.com>
2018-10-17 18:24:17 -0400

    100%

--- humps/__main__.py
+++ humps/__main__.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains all the core logic for humps.
+"""
 import re
 from collections import Mapping
 
@@ -8,55 +12,101 @@ UNDERSCORE_RE = re.compile(r'[\-_\s]+(.?)')
 SPLIT_RE = re.compile(r'([A-Z][^A-Z]*)')
 
 
-def pascalize(obj):
-    if isinstance(obj, (list, Mapping)):
-        return _process_keys(obj, pascalize)
+def pascalize(str_or_iter):
+    """Converts a string, dict, or list of dicts to pascal case.
+
+    :param str_or_iter:
+      A string or iterable.
+
+    """
+    if str(str_or_iter).isnumeric():
+        return str_or_iter
+    elif isinstance(str_or_iter, (list, Mapping)):
+        return _process_keys(str_or_iter, pascalize)
     else:
-        return ''.join([obj[0].upper(), camelize(obj[1:])])
+        return ''.join([str_or_iter[0].upper(), camelize(str_or_iter[1:])])
 
 
-def camelize(obj):
-    if str(obj).isnumeric():
-        return obj
-    elif isinstance(obj, (list, Mapping)):
-        return _process_keys(obj, camelize)
+def camelize(str_or_iter):
+    """Converts a string, dict, or list of dicts to camel case.
+
+    :param str_or_iter:
+      A string or iterable.
+
+    """
+    if str(str_or_iter).isnumeric():
+        return str_or_iter
+    elif isinstance(str_or_iter, (list, Mapping)):
+        return _process_keys(str_or_iter, camelize)
     else:
         return ''.join([
-            obj[0].lower(),
-            UNDERSCORE_RE.sub(lambda m: m.group(1).upper(), obj)[1:],
+            str_or_iter[0].lower(),
+            UNDERSCORE_RE.sub(lambda m: m.group(1).upper(), str_or_iter)[1:],
         ])
 
 
-def decamelize(obj):
-    if isinstance(obj, (list, Mapping)):
-        return _process_keys(obj, decamelize)
+def decamelize(str_or_iter):
+    """Converts a string, dict, or list of dicts to snake case.
+
+    :param str_or_iter:
+      A string or iterable.
+
+    """
+    if str(str_or_iter).isnumeric():
+        return str_or_iter
+    elif isinstance(str_or_iter, (list, Mapping)):
+        return _process_keys(str_or_iter, decamelize)
     else:
-        return separate_words(obj).lower()
+        return separate_words(str_or_iter).lower()
 
 
 def depascalize(string):
+    """Converts a string, dict, or list of dicts to snake case.
+
+    :param str_or_iter:
+      A string or iterable.
+
+    """
     return decamelize(string)
 
 
-def is_camelcase(string):
-    return string == camelize(string)
+def is_camelcase(str_or_iter):
+    """Determines if a string, dict, or list of dicts is camel case.
+
+    :param str_or_iter:
+      A string or iterable.
+
+    """
+    return str_or_iter == camelize(str_or_iter)
 
 
-def is_pascalcase(string):
-    return string == pascalize(string)
+def is_pascalcase(str_or_iter):
+    """Determines if a string, dict, or list of dicts is pascal case.
+
+    :param str_or_iter:
+      A string or iterable.
+
+    """
+    return str_or_iter == pascalize(str_or_iter)
 
 
-def is_snakecase(string):
-    return string == decamelize(string)
+def is_snakecase(str_or_iter):
+    """Determines if a string, dict, or list of dicts is snake case.
+
+    :param str_or_iter:
+      A string or iterable.
+
+    """
+    return str_or_iter == decamelize(str_or_iter)
 
 
-def _process_keys(obj, fn):
-    if isinstance(obj, list):
-        return [_process_keys(k, fn) for k in obj]
-    elif isinstance(obj, Mapping):
-        return {fn(k): _process_keys(v, fn) for k, v in obj.items()}
+def _process_keys(str_or_iter, fn):
+    if isinstance(str_or_iter, list):
+        return [_process_keys(k, fn) for k in str_or_iter]
+    elif isinstance(str_or_iter, Mapping):
+        return {fn(k): _process_keys(v, fn) for k, v in str_or_iter.items()}
     else:
-        return obj
+        return str_or_iter
 
 
 def separate_words(string, separator='_', split=SPLIT_RE):
--- tests/test_humps.py
+++ tests/test_humps.py
@@ -4,11 +4,98 @@ import humps
 def test_converting_strings():
     assert humps.camelize('jack_in_the_box') == 'jackInTheBox'
     assert humps.decamelize('rubyTuesdays') == 'ruby_tuesdays'
+    assert humps.depascalize('UnosPizza') == 'unos_pizza'
     assert humps.pascalize('red_robin') == 'RedRobin'
 
 
-def test_decamelize_keys():
-    camelcase_keys = [
+def test_conditionals():
+    assert humps.is_camelcase('jackInTheBox')
+    assert humps.is_pascalcase('RedRobin')
+    assert humps.is_snakecase('ruby_tuesdays')
+    assert humps.is_camelcase('ruby_tuesdays') is False
+    assert humps.is_snakecase('jackInTheBox') is False
+
+
+def test_numeric():
+    assert humps.camelize(123) == 123
+    assert humps.decamelize(123) == 123
+    assert humps.pascalize(123) == 123
+
+
+def test_camelize():
+    actual = humps.camelize({
+        'videos': [
+            {
+                'fallback_url': 'https://media.io/video',
+                'scrubber_media_url': 'https://media.io/video',
+                'dash_url': 'https://media.io/video',
+            },
+        ],
+        'images': [
+            {
+                'fallback_url': 'https://media.io/image',
+                'scrubber_media_url': 'https://media.io/image',
+                'url': 'https://media.io/image',
+            },
+        ],
+    })
+    expected = {
+        'videos': [
+            {
+                'fallbackUrl': 'https://media.io/video',
+                'scrubberMediaUrl': 'https://media.io/video',
+                'dashUrl': 'https://media.io/video',
+            },
+        ],
+        'images': [
+            {
+                'fallbackUrl': 'https://media.io/image',
+                'scrubberMediaUrl': 'https://media.io/image',
+                'url': 'https://media.io/image',
+            },
+        ],
+    }
+    assert actual == expected
+
+
+def test_pascalize():
+    actual = humps.pascalize({
+        'videos': [
+            {
+                'fallback_url': 'https://media.io/video',
+                'scrubber_media_url': 'https://media.io/video',
+                'dash_url': 'https://media.io/video',
+            },
+        ],
+        'images': [
+            {
+                'fallback_url': 'https://media.io/image',
+                'scrubber_media_url': 'https://media.io/image',
+                'url': 'https://media.io/image',
+            },
+        ],
+    })
+    expected = {
+        'Videos': [
+            {
+                'FallbackUrl': 'https://media.io/video',
+                'ScrubberMediaUrl': 'https://media.io/video',
+                'DashUrl': 'https://media.io/video',
+            },
+        ],
+        'Images': [
+            {
+                'FallbackUrl': 'https://media.io/image',
+                'ScrubberMediaUrl': 'https://media.io/image',
+                'Url': 'https://media.io/image',
+            },
+        ],
+    }
+    assert actual == expected
+
+
+def test_decamelize():
+    actual = humps.decamelize([
         {
             'symbol': 'AAL',
             'lastPrice': 31.78,
@@ -27,7 +114,7 @@ def test_decamelize_keys():
             'lastPrice': 25.42,
             'impliedVolatality': 0.4454,
         },
-    ]
+    ])
     expected = [
         {
             'symbol': 'AAL',
@@ -49,4 +136,4 @@ def test_decamelize_keys():
         },
     ]
 
-    assert humps.decamelize(camelcase_keys) == expected
+    assert actual == expected
