02db4bf61f674cfbc2c31743e20b17b317c2f16c
Nick Ficano <nficano@gmail.com>
2018-10-18 19:17:57 -0400

    Bump version: 1.0.17 → 1.0.18

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ with open('README.md') as fh:
 
 setup(
     name='pyhumps',
-    version='1.0.17',
+    version='1.0.18',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['humps'],
