28c101d3f94f5e9e2f1677564aa56f7c748ff3d2
Nick Ficano <nficano@gmail.com>
2018-10-23 13:02:23 -0400

    moved compat into hump

--- humps/humps.py
+++ humps/humps.py
@@ -3,9 +3,21 @@
 This module contains all the core logic for humps.
 """
 import re
+import sys
+
 from collections import Mapping
 
-from humps.compat import str
+_ver = sys.version_info
+is_py2 = (_ver[0] == 2)
+is_py3 = (_ver[0] == 3)
+
+
+if is_py2:
+    str = unicode
+
+
+if is_py3:
+    str = str
 
 
 UNDERSCORE_RE = re.compile(r'[\-_\s]+(.?)')
