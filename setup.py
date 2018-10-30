7a2f643ab7d0ab9eacd1b308193f4cc7d62b36a1
Nick Ficano <nficano@gmail.com>
2018-10-30 05:10:57 -0400

    Bump version: 1.2.1 → 1.2.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ class UploadCommand(Command):
 
 setup(
     name='pyhumps',
-    version='1.2.1',
+    version='1.2.2',
     author='Nick Ficano',
     author_email='nficano@gmail.com',
     packages=['humps'],
