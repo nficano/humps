61bed7b13d221fd950ebb12abbba147558b84f10
Ronnie Llamado <llamado.ronnie@gmail.com>
2021-10-07 14:02:22 -0700

    Drop support for Python 2.7

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setuptools.setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
