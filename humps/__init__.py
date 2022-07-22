553423c13fc15bfbad4283ec31606c6c2cfeac18
Will Frey <jfrey89@gmail.com>
2022-07-22 17:45:03 -0400

    Explicitly re-export symbols in __init__.py

--- humps/__init__.py
+++ humps/__init__.py
@@ -8,6 +8,18 @@ __version__ = "3.7.2"
 __author__ = "Nick Ficano"
 __license__ = "Unlicense License"
 __copyright__ = "Copyright 2019 Nick Ficano"
+__all__ = [
+    "camelize",
+    "decamelize",
+    "kebabize",
+    "dekebabize",
+    "depascalize",
+    "is_camelcase",
+    "is_pascalcase",
+    "is_kebabcase",
+    "is_snakecase",
+    "pascalize",
+]
 
 from humps.main import (
     camelize,
