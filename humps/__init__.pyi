0dd91d6395e9b1665ea503219e4164dd5919ad59
Sebastian Kreft <skreft@gmail.com>
2022-07-30 17:27:28 -0400

    fix(types): add __init__.pyi
    Without this file, one would get the following error when `implicit_reexport` is disabled.
    
    ```
    path:10: error: Module has no attribute "camelize"; maybe "decamelize"?  [attr-defined]
    ```

---
+++ humps/__init__.pyi
@@ -0,0 +1,25 @@
+from humps.main import (
+    camelize,
+    decamelize,
+    kebabize,
+    dekebabize,
+    depascalize,
+    is_camelcase,
+    is_pascalcase,
+    is_kebabcase,
+    is_snakecase,
+    pascalize,
+)
+
+__all__ = [
+    camelize,
+    decamelize,
+    kebabize,
+    dekebabize,
+    depascalize,
+    is_camelcase,
+    is_pascalcase,
+    is_kebabcase,
+    is_snakecase,
+    pascalize,
+]
--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     author="Nick Ficano",
     author_email="nficano@gmail.com",
     packages=["humps"],
-    package_data={"": ["LICENSE"], "humps": ["py.typed", "main.pyi"]},
+    package_data={"": ["LICENSE"], "humps": ["py.typed", "__init__.pyi", "main.pyi"]},
     url="https://github.com/nficano/humps",
     license="The Unlicense (Unlicense)",
     classifiers=[
